9429e45577af4254521650f2b4fe6b68a34e61b6
Arulalan <arulalananbazhagan@gmail.com>
2023-10-20 17:07:20 +0530

    htmlcss

---
+++ src/org/basics/Classes.java
@@ -0,0 +1,34 @@
+package org.basics;
+
+public class Classes {
+
+		public static void main(String[] args) {
+			
+		
+		System.out.println("Java executed successfully");
+		
+		int a =12345 , b, m=0;
+		
+		while(a>0)
+		{
+			b=a%10;
+			m=m+b;
+			a=a/10;
+			
+		}
+		System.out.println("The sum numbers are "+m);
+		
+		System.out.println("Synecron company are the one with the basic process");
+		
+		System.out.println("Asynchronous error");
+		
+		System.out.println("Binary sector issues");
+		
+	    
+
+	}
+
+	}
+
+
+
